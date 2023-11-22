a2e705dcde6abe80466e66179abddac62d8faf67
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-11-22 18:28:41 +0100

    Create base project for ae_asistencia

---
+++ parent-project/ae_asistencia/src/main/java/iso/b05/App.java
@@ -0,0 +1,13 @@
+package iso.b05;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
