f41ce7418f40b616ed5a64f6223efb42d1262666
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-12-15 09:34:23 +0100

    Add code template for gestion encuestas

---
+++ parent-project/server/src/main/java/dominioGestionEncuestasDocentesServidor/EncuestaDocente.java
@@ -0,0 +1,18 @@
+package dominioGestionEncuestasDocentesServidor;
+
+/**
+ * Clase de dominio para mantener la información relativa a las encuestas
+ */
+public class EncuestaDocente {
+	private String id;
+	private int valoracionMedia;
+	private int valoraciones;
+
+	public EncuestaDocente(String id) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void valorar(int valoracion) {
+		throw new UnsupportedOperationException();
+	}
+}
