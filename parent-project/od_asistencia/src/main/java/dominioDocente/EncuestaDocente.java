e6b817495743c71ec058398bfcff7884806cf635
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-12-15 09:27:55 +0100

    Add code template for od encuestas 0.2.0

---
+++ parent-project/od_asistencia/src/main/java/dominioDocente/EncuestaDocente.java
@@ -0,0 +1,20 @@
+package dominioDocente;
+
+/**
+ * Clase de dominio que mantiene la información sobre las encuestas
+ */
+public class EncuestaDocente {
+	private String id;
+	private int valoracionMedia;
+
+	public EncuestaDocente(String id) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void valorar(int valoracion) {
+		throw new UnsupportedOperationException();
+		/*
+		 * estoy cansado jefe no quiero seguir trabajando
+		 */
+	}
+}
