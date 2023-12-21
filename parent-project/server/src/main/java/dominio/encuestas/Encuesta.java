6d2d678fcf5e0eb62650f5bd37eaf3343ad2943a
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-12-21 14:37:09 +0100

    Add code template of it 4 encuestas informativas

--- parent-project/server/src/main/java/dominio/encuestas/Encuesta.java
+++ parent-project/server/src/main/java/dominio/encuestas/Encuesta.java
@@ -1,14 +1,15 @@
-package dominioGestionEncuestasDocentesServidor;
+package dominio.encuestas;
 
 /**
  * Clase de dominio para mantener la información relativa a las encuestas
  */
-public class EncuestaDocente {
+public class Encuesta {
 	private String id;
 	private int valoracionMedia;
 	private int valoraciones;
+	private TIPOENCUESTA tipo;
 
-	public EncuestaDocente(String id) {
+	public Encuesta(String id) {
 		throw new UnsupportedOperationException();
 	}
 
